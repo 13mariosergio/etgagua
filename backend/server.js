5fd43714ba0158c9d20e1366935cf8bce8ee994e
13mariosergio <mario1saude@gmail.comm>
2026-02-01 08:36:48 -0300

    fix: remover import do SQLite

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const db = require("./db");
 const bcrypt = require("bcrypt");
 const { signToken, requireAuth, requireRole } = require("./auth");
 const { initDB, getDB } = require("./db-postgres");
